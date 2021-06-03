d25f191673bcb19fb8320126434434b0417a528e
Ashwin-2020 <85284641+Ashwin-2020@users.noreply.github.com>
2021-06-03 15:17:50 +0530

    ascode.py

---
+++ Ashcode.py
@@ -0,0 +1,14 @@
+print(Hello guys)
+CI=89898
+code='ash@code'
+ID=input(Customer ID:)
+if CI==ID:
+  bass=input(Password:)
+  if code==bass:
+    print(Fetching account details)
+  else:
+    print(password is wrong)
+else:
+  print(Your Customer ID is wrong)
+  
+  
